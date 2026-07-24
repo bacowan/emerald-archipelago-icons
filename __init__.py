276b23d810b88f7da80bba5368671b52cea5baa8
bacowan <cowan.brendan.a@gmail.com>
2026-07-24 23:40:18 +0900

    extracting a function

--- __init__.py
+++ __init__.py
@@ -2,65 +2,75 @@ import asyncio
 import websockets
 import json
 import Utils
+from dataclasses import dataclass
 from worlds.LauncherComponents import Component, components, Type
 
+@dataclass
+class LocationItemMapping:
+    game: str
+    item_name: str
+    location_id: int
+
+async def get_locations_to_items(address: str, slot_name: str, password: str) -> list[LocationItemMapping]:
+    async with websockets.connect(f"ws://{address}", ping_timeout=None, ping_interval=None, max_size=None) as ws:
+        # Server sends RoomInfo first
+        room_info = json.loads(await ws.recv())
+        await ws.send(json.dumps([{"cmd": "GetDataPackage", "games": room_info[0]["games"]}]))
+        data = json.loads(await ws.recv())
+
+        await ws.send(json.dumps([{
+            "cmd": "Connect",
+            "password": password,
+            "game": "Pokemon Emerald",
+            "name": slot_name,
+            "version": {
+                "major": Utils.__version__.split(".")[0],
+                "minor": Utils.__version__.split(".")[1],
+                "build": Utils.__version__.split(".")[2],
+                "class": "Version",
+            },
+            "items_handling": 0b000,  # don't worry about sending items as they come in since this is a one time script
+            "tags": ["NoText"],
+            "uuid": Utils.get_unique_identifier()
+        }]))
+
+        connection_results = json.loads(await ws.recv())
+        if connection_results[0]["cmd"] != "Connected":
+            raise Exception("Connection failed")
+
+        location_name_to_id = data[0]['data']['games']['Pokemon Emerald']['location_name_to_id']
+        pokedex_location_ids = [value for key, value in location_name_to_id.items() if key.startswith("Pokedex")]
+
+        await ws.send(json.dumps([{"cmd": "LocationScouts", "locations": pokedex_location_ids, "create_as_hint": 0}]))
+        try:
+            network_locations = json.loads(await ws.recv())
+        except:
+            # TODO: server doesn't send a response when any of the location checks aren't set
+            raise
+
+        player_to_game = {int(slot): info['game'] for slot, info in connection_results[0]['slot_info'].items()}
+        location_ids_to_names = {
+            game: {
+                item_id: item_name
+                for item_name, item_id in value['item_name_to_id'].items()
+            }
+            for game, value in data[0]['data']['games'].items()
+        }
+
+        def network_location_to_item(network_location):
+            game = player_to_game[network_location['player']]
+            return LocationItemMapping(
+                game=game,
+                item_name=location_ids_to_names[game][network_location['item']],
+                location_id=network_location['location']
+            )
+
+        return [network_location_to_item(network_location) for network_location in network_locations[0]['locations']]
 
 def apply_patch():
     async def main(address: str, slot_name: str, password: str):
-        async with websockets.connect(f"ws://{address}", ping_timeout=None, ping_interval=None, max_size=None) as ws:
-            # Server sends RoomInfo first
-            room_info = json.loads(await ws.recv())
-            await ws.send(json.dumps([{"cmd": "GetDataPackage", "games": room_info[0]["games"]}]))
-            data = json.loads(await ws.recv())
-
-            await ws.send(json.dumps([{
-                "cmd": "Connect",
-                "password": password,
-                "game": "Pokemon Emerald",
-                "name": slot_name,
-                "version": {
-                    "major": Utils.__version__.split(".")[0],
-                    "minor": Utils.__version__.split(".")[1],
-                    "build": Utils.__version__.split(".")[2],
-                    "class": "Version",
-                },
-                "items_handling": 0b000, # don't worry about sending items as they come in since this is a one time script
-                "tags": ["NoText"],
-                "uuid": Utils.get_unique_identifier()
-            }]))
-
-            connection_results = json.loads(await ws.recv())
-            if connection_results[0]["cmd"] != "Connected":
-                raise Exception("Connection failed")
-
-            location_name_to_id = data[0]['data']['games']['Pokemon Emerald']['location_name_to_id']
-            pokedex_location_ids = [value for key, value in location_name_to_id.items() if key.startswith("Pokedex")]
-
-            await ws.send(json.dumps([{"cmd": "LocationScouts", "locations": pokedex_location_ids, "create_as_hint": 0}]))
-            try:
-                network_locations = json.loads(await ws.recv())
-            except:
-                # TODO: server doesn't send a response when any of the location checks aren't set
-                raise
-
-            player_to_game = {int(slot): info['game'] for slot, info in connection_results[0]['slot_info'].items()}
-            location_ids_to_names = {
-                game: {
-                    item_id: item_name
-                    for item_name, item_id in value['item_name_to_id'].items()
-                }
-                for game, value in data[0]['data']['games'].items()
-            }
-            def network_location_to_item(network_location):
-                game = player_to_game[network_location['player']]
-                return {
-                    'location': network_location['location'],
-                    'item_name': location_ids_to_names[game][network_location['item']],
-                    'game': game
-                }
-
-            pokemon_to_items = [network_location_to_item(network_location) for network_location in network_locations[0]['locations']]
-            pass
+        locations_to_items = await get_locations_to_items(address, slot_name, password)
+        
 
     address = input("Server address (e.g. archipelago.gg:38281): ").strip()
     slot_name = input("Slot name: ").strip()
