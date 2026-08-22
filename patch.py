1f63defba6a659d8618fdc65c0943a272b06a6e0
bacowan <5496948+bacowan@users.noreply.github.com>
2026-08-22 19:42:16 +0900

    patching level up moves

--- patch.py
+++ patch.py
@@ -2,7 +2,7 @@ import json
 import pkgutil
 
 from pokemon import Pokemon
-from worlds.pokemon_emerald_icons.pokemon import Moveset
+from worlds.pokemon_emerald_icons.pokemon import Moveset, LevelUpMove
 
 rom_locations = json.loads(pkgutil.get_data(__name__, "data/rom_locations.json").decode("utf-8-sig"))
 characters = json.loads(pkgutil.get_data(__name__, "data/characters.json").decode("utf-8-sig"))
@@ -14,10 +14,22 @@ base_sprite_table_offset = int(rom_locations["offsets"]["sprite_table"], 0)
 sprite_table_entry_length = int(rom_locations["lengths"]["sprite_table_entry"], 0)
 base_palette_table_offset = int(rom_locations["offsets"]["palette_table"], 0)
 palette_table_entry_length = int(rom_locations["lengths"]["palette_table_entry"], 0)
+moves_learnable_offset = int(rom_locations["offsets"]["moves_learnable"], 0)
+level_up_move_size = int(rom_locations["sizes"]["level_up_move"], 0)
 
 
-def _patch_moveset(rom_data: bytearray, pokemon: Pokemon):
-    pass
+def _patch_moveset(rom_data: bytearray, pokemon: Pokemon, free_space_start: int) -> int:
+    # write the new level up moves
+    for move in pokemon.moveset.level_up_moves:
+        # Note that the format for moves is the first 9 bits are the move id, and the next 7 are the level,
+        # hence the bit shift of 9
+        move_bytes = (move.level << 9 | move.move_id).to_bytes(level_up_move_size, byteorder="little")
+        rom_data[free_space_start:free_space_start+level_up_move_size] = move_bytes
+        free_space_start += level_up_move_size
+    # add terminator characters
+    rom_data[free_space_start:free_space_start+level_up_move_size] = 0xFF_FF.to_bytes(level_up_move_size)
+    free_space_start += level_up_move_size
+    return free_space_start
 
 def _patch_sprite(rom_data: bytearray, pokemon: Pokemon, free_space_start: int) -> int:
     # write the new sprite data
@@ -50,7 +62,7 @@ def _patch_single_pokemon(rom_data: bytearray, pokemon: Pokemon, free_space_star
     _patch_name(rom_data, pokemon)
     free_space_start = _patch_sprite(rom_data, pokemon, free_space_start)
     free_space_start = _patch_palette(rom_data, pokemon, free_space_start)
-    _patch_moveset(rom_data, pokemon)
+    free_space_start = _patch_moveset(rom_data, pokemon, free_space_start)
     return free_space_start
 
 def patch(rom_data: bytearray, pokemon: list[Pokemon]) -> None:
